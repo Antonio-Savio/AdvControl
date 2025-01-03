b8379f521a82f38b17f25d95d82be713b4ddc78f
Antonio-Savio <antonio.savio28@gmail.com>
2025-01-03 05:17:41 -0300

    style: change font to Open Sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans } from 'next/font/google'
 import "./globals.css";
 import { Header } from "@/components/header";
 import { AuthProvider } from '@/providers/auth'
@@ -8,15 +8,11 @@ import { Toaster } from "react-hot-toast";
 import { Suspense } from "react";
 import Loading from "./loading";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const opensans = Open_Sans({
+  display: 'swap',
+  fallback: ['sans-serif'],
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "AdvControl",
@@ -31,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={opensans.className}
       >
         <AuthProvider>
           <Toaster
