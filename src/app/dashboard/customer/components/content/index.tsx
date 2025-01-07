aa664d77162ae333579f1973b2a25e9f8b82c6db
Antonio-Savio <antonio.savio28@gmail.com>
2025-01-07 01:11:57 -0300

    refactor: create a new content component in order to use Suspense UI

---
+++ src/app/dashboard/customer/components/content/index.tsx
@@ -0,0 +1,50 @@
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import prismaClient from "@/lib/prisma";
+import { Container } from "@/components/container";
+import Link from "next/link";
+import { Card } from "../card";
+
+export async function Content(){
+    const session = await getServerSession(authOptions)
+    
+    if(!session || !session.user){
+        redirect('/')
+    }
+
+    const customers = await prismaClient.customer.findMany({
+        where: {
+            userId: session.user.id
+        },
+        orderBy: {
+            created_at: 'desc'
+        }
+    })
+
+    return(
+        <Container>
+            <main>
+                <div className="flex items-center justify-between">
+                    <h1 className="text-3xl font-bold">Meus clientes</h1>
+                    <Link href='/dashboard/customer/new' className="bg-gray-900 text-white rounded px-4 py-1">
+                        Novo cliente
+                    </Link>
+                </div>
+
+                <section className="my-4 grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+                    {customers.map( customer => (
+                        <Card 
+                            key={customer.id} 
+                            customer={customer}
+                        />
+                    ))}
+                </section>
+
+                {customers.length === 0 && (
+                    <h2 className="font-bold text-gray-600">Nenhum cliente cadastrado.</h2>
+                )}
+            </main>
+        </Container>
+    )
+}
--- src/app/dashboard/customer/page.tsx
+++ src/app/dashboard/customer/page.tsx
@@ -1,50 +1,12 @@
-import { Container } from "@/components/container";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth"
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { Card } from "./components/card";
-import prismaClient from "@/lib/prisma";
+import { Content } from "./components/content";
+import { Suspense } from "react";
+import Loading from "@/app/dashboard/loading"
 
 export default async function Customer(){
-    const session = await getServerSession(authOptions)
     
-    if(!session || !session.user){
-        redirect('/')
-    }
-
-    const customers = await prismaClient.customer.findMany({
-        where: {
-            userId: session.user.id
-        },
-        orderBy: {
-            created_at: 'desc'
-        }
-    })
-
     return(
-        <Container>
-            <main>
-                <div className="flex items-center justify-between">
-                    <h1 className="text-3xl font-bold">Meus clientes</h1>
-                    <Link href='/dashboard/customer/new' className="bg-gray-900 text-white rounded px-4 py-1">
-                        Novo cliente
-                    </Link>
-                </div>
-
-                <section className="my-4 grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
-                    {customers.map( customer => (
-                        <Card 
-                            key={customer.id} 
-                            customer={customer}
-                        />
-                    ))}
-                </section>
-
-                {customers.length === 0 && (
-                    <h2 className="font-bold text-gray-600">Nenhum cliente cadastrado.</h2>
-                )}
-            </main>
-        </Container>
+        <Suspense fallback={<Loading/>}>
+            <Content/>
+        </Suspense>
     )
 }
